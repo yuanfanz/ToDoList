7d7316ad97b897f9f9a000fc19d58550e39f7bdc
yuanfanz <yuanfanz@uci.edu>
2016-12-25 23:01:44 -0800

    .

--- app/src/main/java/yuanfanz/todolist/MainActivity.java
+++ app/src/main/java/yuanfanz/todolist/MainActivity.java
@@ -1,7 +1,17 @@
 package yuanfanz.todolist;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateUtils;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import yuanfanz.todolist.models.Todo;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupUI(mockData());
+    }
+
+    private void setupUI (@NonNull List<Todo> todos) {
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.list_container);
+        linearLayout.removeAllViews();
+        for (Todo todo : todos) {
+            View view = getListItemView(todo);
+            linearLayout.addView(view);
+        }
+    }
+
+    @NonNull
+    private View getListItemView(@NonNull Todo todo) {
+        View view = getLayoutInflater().inflate(R.layout.todo_list_item, null);
+        ((TextView) view.findViewById(R.id.todo_list_item_text)).setText(todo.text);
+        return view;
+    }
+
+    @NonNull
+    private List<Todo> mockData() {
+        List<Todo> list = new ArrayList<>();
+        for (int i = 0; i < 1000; ++i) {
+            list.add(new Todo("todo " + i));
+        }
+        return list;
     }
 }
---
+++ app/src/main/java/yuanfanz/todolist/models/Todo.java
@@ -0,0 +1,16 @@
+package yuanfanz.todolist.models;
+
+/**
+ * Created by yuanfanz on 16/12/25.
+ */
+
+public class Todo {
+
+    public boolean done;
+
+    public String text;
+
+    public Todo(String text) {
+        this.text = text;
+    }
+}
